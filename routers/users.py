e242abcf39674a196c0987da25bd619e645dc431
saksham <168113647+sxkshmgit@users.noreply.github.com>
2026-09-25 12:13:17 +0530

    add jwt authentication

--- routers/users.py
+++ routers/users.py
@@ -1,13 +1,31 @@
-from fastapi import APIRouter
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+
+import jwt
+from fastapi import APIRouter, Depends, HTTPException, status
 from pwdlib import PasswordHash
+from sqlmodel import select
 
 from database import SessionDep
 from models import User, UserCreate, UserRead
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
+
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 password_hash = PasswordHash.recommended()
 
+SECRET_KEY = "CHANGE_THIS_TO_A_RANDOM_SECRET_IN_PRODUCTION"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/token")
+
+def create_access_token(data: dict, expires_delta: timedelta) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 def hash_password(password: str) -> str:
     return password_hash.hash(password)
@@ -29,3 +47,46 @@ def create_user(user: UserCreate, session: SessionDep):
     session.commit()
     session.refresh(new_user)
     return new_user
+@router.post("/token")
+def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: SessionDep,
+):
+    user = session.exec(select(User).where(User.username == form_data.username)).first()
+
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token = create_access_token(
+        data={"sub": user.username},
+        expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    session: SessionDep,
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except jwt.PyJWTError:
+        raise credentials_exception
+
+    user = session.exec(select(User).where(User.username == username)).first()
+    if user is None:
+        raise credentials_exception
+
+    return user
